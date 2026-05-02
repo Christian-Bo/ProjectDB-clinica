120e9a55b5e824b699897af043a1392e91658a44
Christian-Bo <cordonezx@miumg.edu.gt>
2026-05-02 07:04:53 -0600

    Disable authentication in Program.cs
    Comment out authentication middleware and related configuration.

--- src/Clinica.API/Program.cs
+++ src/Clinica.API/Program.cs
@@ -34,21 +34,21 @@ builder.Services.Configure<TicketQueueWorkerOptions>(
 
 builder.Services.AddInfrastructure();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer           = true,
-            ValidateAudience         = true,
-            ValidateLifetime         = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer              = builder.Configuration["Jwt:Issuer"],
-            ValidAudience            = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey         = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
-        };
-    });
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//    .AddJwtBearer(options =>
+//    {
+//        options.TokenValidationParameters = new TokenValidationParameters
+//        {
+//            ValidateIssuer           = true,
+//            ValidateAudience         = true,
+//            ValidateLifetime         = true,
+//            ValidateIssuerSigningKey = true,
+//            ValidIssuer              = builder.Configuration["Jwt:Issuer"],
+//            ValidAudience            = builder.Configuration["Jwt:Audience"],
+//            IssuerSigningKey         = new SymmetricSecurityKey(
+//                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+//        };
+//    });
 
 builder.Services.AddAuthorization();
 builder.Services
@@ -122,7 +122,7 @@ app.UseSwaggerUI();
 
 app.UseMiddleware<GlobalExceptionMiddleware>();
 app.UseCors("ClinicaPolicy");
-app.UseAuthentication();    
+//app.UseAuthentication();    
 app.UseAuthorization();
 app.MapControllers();
 
